153fbcba905289d5670724f4b7a138965a64ae29
Matt Luna <mat.c.luna@gmail.com>
2021-10-16 10:22:36 -0500

    PromptBuild

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const Manager = require('./lib/Manager.js');
 const Intern = require('./lib/Intern.js');
 const Engineer = require('./lib/Engineer.js');
 
-var employeeData = {}
+var employeeData = [];
 
-function EmployeeData() {
+function Profile() {
     this.name;
     this.id;
     this.email;
@@ -17,7 +17,8 @@ const empName = () => {
   if(!employeeData) {
     employeeData = {};
   }
-  return inquirer 
+  //Do I return inquirer, or do I omit the return? The jest-another-RPG omits the return
+  inquirer 
     .prompt([
       {
         type: "input",
@@ -32,14 +33,70 @@ const empName = () => {
           }
         },
       },
+      {
+        type: "list",
+        name: "role",
+        message: "What position does this employee have?",
+        choices: ["Employee", "Engineer", "Manager", "Intern"]
+      },
+      {
+        type: "number",
+        name: "id",
+        message: "What is your employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+      }
   ])
-  .then((empName) => {
-    console.log("this is the employee name");
-    console.log(empName);
-    return empName;
-  });
+  .then(({ name, role, id, email}) => {
+    this.employee = new Employee(name, id, email);
+
+    if(role === "Manager") {
+      inquirer
+        .prompt({
+          type: "number",
+          name: "officeNumb",
+          message: "What is the office number of this manager?",
+        })
+        .then(({ officeNumb }) => {
+          this.manager = new Manager(officeNumb);
+          employeeData.push(this.employee);
+        })
+    } else if (role ==="Engineer") {
+      inquirer
+        .prompt({
+          type: "input",
+          name: "github",
+          message: "What is the github for this engineer?",
+        })
+        .then(({ github }) => {
+          this.engineer = new Engineer(github);
+          employeeData.push(this.employee);
+        })
+    } else if (role === "Intern") {
+            inquirer
+        .prompt({
+          type: "input",
+          name: "school",
+          message: "What is the school for this intern?",
+        })
+        .then(({ school }) => {
+          this.intern = new Intern(school);
+
+          employeeData.push(this.employee);
+
+        })
+    } else {
+      employeeData.push(this.employee);
+    }
+
+  })
 };
 
-const role = (name) => {
 
-}
+//To add a new employee prompt
+ inquirer.prompt({});
+
+empName();
